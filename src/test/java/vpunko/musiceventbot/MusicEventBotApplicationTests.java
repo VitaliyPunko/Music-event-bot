52cdd2257b2133fcef5f4d35df85c50b39f440dd
vitaliy_punko <sapoghnik23@gmail.com>
2025-02-28 14:29:27 +0300

    First commit~

---
+++ src/test/java/vpunko/musiceventbot/MusicEventBotApplicationTests.java
@@ -0,0 +1,13 @@
+package vpunko.musiceventbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicEventBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
