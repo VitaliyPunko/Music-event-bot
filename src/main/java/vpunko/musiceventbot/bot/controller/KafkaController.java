c96678991dd01d24fd2b37e6c5466c1314954f10
vitaliy_punko <sapoghnik23@gmail.com>
2025-11-09 21:06:28 +0300

    - create k8s files - set env variables from configMap.yaml and secret - little refactoring

--- src/main/java/vpunko/musiceventbot/bot/controller/KafkaController.java
+++ src/main/java/vpunko/musiceventbot/bot/controller/KafkaController.java
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import vpunko.musiceventbot.bot.dto.User;
-import vpunko.musiceventbot.bot.service.KafkaSender;
+import vpunko.musiceventbot.bot.publisher.MusicCoreKafkaPublisher;
 
 @RestController
 public class KafkaController {
 
     @Autowired
-    private KafkaSender kafkaSender;
+    private MusicCoreKafkaPublisher musicCoreKafkaPublisher;
 
     @GetMapping("/send/{message}")
     public String sendMessage(@PathVariable String message) {
         User user = new User(message);
-        kafkaSender.sendMessage(user);
+        musicCoreKafkaPublisher.sendMessage(user);
         return message;
     }
 }
