2379e845681baf15be16521b457e7a94c2ce8e50
vitaliy_punko <sapoghnik23@gmail.com>
2025-11-23 18:43:42 +0300

    change deployment.yaml resources to scale pods

--- src/main/java/vpunko/musiceventbot/bot/MusicEventBot.java
+++ src/main/java/vpunko/musiceventbot/bot/MusicEventBot.java
@@ -147,7 +147,7 @@ public class MusicEventBot extends TelegramWebhookBot {
 
     @Override
     public String getBotPath() {
-        return "webhook";
+        return "/webhook";
     }
 
 //    @Override
